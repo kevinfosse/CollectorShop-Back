c87ac9ddfed72cb4b87e53a9a5c1fa79ef85f724
Thomas NISI <nisi.thomas@outlook.fr>
2026-02-18 11:37:41 +0100

    feat(migrations): add soft delete columns migration
    chore(seed): create seed data script for categories, brands, and products

---
+++ CollectorShop.Infrastructure/Data/Migrations/20260218100051_AddSoftDeleteColumns.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CollectorShop.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSoftDeleteColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
